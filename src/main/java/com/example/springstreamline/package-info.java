cbcd13fff3002b86172b1ae564c13d3783751eec
Notoriousjayy <38364814+Notoriousjayy@users.noreply.github.com>
2024-11-27 14:33:56 -0500

    Update package-info.java

--- src/main/java/com/example/springstreamline/package-info.java
+++ src/main/java/com/example/springstreamline/package-info.java
@@ -1,4 +1,5 @@
 /**
- * This package contains the main entry point and core components for the SpringStreamline application.
+ * This package contains the main entry point and core components
+ * for the SpringStreamline application.
  */
 package com.example.springstreamline;
